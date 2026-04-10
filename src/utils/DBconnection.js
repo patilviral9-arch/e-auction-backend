899fd390b1ffb314008409085ae04449953bea23
viral <patilviral9@gmail.com>
2026-04-10 11:54:57 +0530

    new

--- src/utils/DBconnection.js
+++ src/utils/DBconnection.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const DBconnection = ()=>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then(()=>{
-        
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}
+const DBconnection = () => {
+    const mongoUrl = String(process.env.MONGO_URL || "").trim();
+    if (!mongoUrl) {
+        console.error("[DB] MONGO_URL is missing in .env");
+        return;
+    }
 
-module.exports = DBconnection
+    mongoose
+        .connect(mongoUrl, {
+            serverSelectionTimeoutMS: Number(process.env.MONGO_SERVER_SELECTION_TIMEOUT_MS || 10000),
+        })
+        .then(() => {
+            console.log("[DB] MongoDB connected");
+        })
+        .catch((err) => {
+            console.error("[DB] MongoDB connection failed:", err.message);
+        });
+};
+
+module.exports = DBconnection;
